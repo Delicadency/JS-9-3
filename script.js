ff262ef10544d8fb1e288a4c3c8deae035be3be2
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-30 22:04:27 +0200

    deleted comment

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Stwórz przycisk, o atrybucie id=”startInterval”. Obok lub pod nim dodaj tag h1, który początkowo ma mieć w sobie tylko cyfrę “0” oraz id=”counter”. 
-Po kliknięciu w przycisk, uruchom funkcję interwału, która co sekundę będzie zwiększać licznik zaczynając od 0 i aktualizować zawartość H1, */
 const createCounter = () => {
   const button = document.createElement("button");
   button.innerText = "Zacznij odliczanie";
